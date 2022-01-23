134fe24b1d0883a0b1ed5b233d700cb0b07d9532
ikehikeh151 <ikehikeh151@gmail.com>
2022-01-24 01:21:59 +0700

    add spinner vehicles-page

--- src/pages/Vehicles/Vehicles.js
+++ src/pages/Vehicles/Vehicles.js
@@ -11,6 +11,7 @@ import "slick-carousel/slick/slick-theme.css"
 // import zeep from "../../assets/images/zeep.webp"
 // import matic from "../../assets/images/matic.webp"
 import Footer from "../../components/Footer"
+import Loading from "../../components/Loading"
 // import Modal from "../../components/Modal"
 
 function Vehicles() {
@@ -158,153 +159,156 @@ function Vehicles() {
   return (
     <main>
       <Header />
-      {isOk}
-      <div className="container px-4 mb-5">
-        <div className="col-lg-12 col-sm-12 col-md-12 justify-content-center d-flex">
-          <div className="col-lg-12 col-sm-12 col-md-12  border rounded-3 mt-5 container-input">
-            <input
-              typeof="search"
-              className="input-search-type"
-              placeholder="Search vehicle (ex. cars, cars name)"
-            />
-            <button typeof="submit" className="btn-search-type float-end">
-              <i className="fas fa-search"></i>
-            </button>
+      {isOk ? (
+        <div className="container px-4 mb-5">
+          <div className="col-lg-12 col-sm-12 col-md-12 justify-content-center d-flex">
+            <div className="col-lg-12 col-sm-12 col-md-12  border rounded-3 mt-5 container-input">
+              <input
+                typeof="search"
+                className="input-search-type"
+                placeholder="Search vehicle (ex. cars, cars name)"
+              />
+              <button typeof="submit" className="btn-search-type float-end">
+                <i className="fas fa-search"></i>
+              </button>
+            </div>
+          </div>
+
+          {/* Popular */}
+          <h3 className="mb-5 mt-5 f-playfair-main">
+            Popular in town
+            <Link to={`${url}/popular`} className="float-end link-view-all">
+              View all <i className="fas fa-chevron-right text-center ms-2"></i>
+            </Link>
+          </h3>
+
+          {/* Card */}
+          <div className="row position-relative mb-lg-5 wrapper-card">
+            {vehiclesPopular.map((item, idx) => {
+              return (
+                <div key={idx} className="col-lg-3 col-md-6 card  ">
+                  <Link
+                    to={`${url}/popular/detail/${item.id}`}
+                    className="wrapper-img-home">
+                    <img
+                      src={`${hostBackend}${item.photo}`}
+                      className="img-size"
+                      alt="van_login"
+                    />
+                    <div className="city-type-vehicles  bg-light rounded shadow ">
+                      <p className="mt-3">
+                        {item.vehicle} <br />
+                        <span className="color-subtitle">{item.location}</span>
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
+
+          {/* Bike */}
+          <h3 className="mb-5 mt-5 f-playfair-main">
+            Bike
+            <Link to={`${url}/bike`} className="float-end link-view-all">
+              View all <i className="fas fa-chevron-right text-center ms-2"></i>
+            </Link>
+          </h3>
+
+          {/* Card */}
+          <div className="row position-relative mb-lg-5 wrapper-card">
+            {vehiclesBike.map((item, idx) => {
+              return (
+                <div key={idx} className="col-lg-3 col-md-6 card  ">
+                  <Link
+                    to={`${url}/bike/detail/${item.id}`}
+                    className="wrapper-img-home">
+                    <img
+                      src={`${hostBackend}${item.photo}`}
+                      className="img-size"
+                      alt="van_login"
+                    />
+                    <div className="city-type-vehicles  bg-light rounded shadow ">
+                      <p className="mt-3">
+                        {item.name} <br />
+                        <span className="color-subtitle">{item.location}</span>
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
+
+          {/* MotorBike */}
+          <h3 className="mb-5 mt-5 f-playfair-main">
+            MotorBike
+            <Link to={`${url}/motorbike`} className="float-end link-view-all">
+              View all <i className="fas fa-chevron-right text-center ms-2"></i>
+            </Link>
+          </h3>
+
+          {/* Card */}
+          <div className="row position-relative mb-lg-5 wrapper-card">
+            {vehiclesMotorBike.map((item, idx) => {
+              return (
+                <div key={idx} className="col-lg-3 col-md-6 card  ">
+                  <Link
+                    to={`${url}/motorbike/detail/${item.id}`}
+                    className="wrapper-img-home">
+                    <img
+                      src={`${hostBackend}${item.photo}`}
+                      className="img-size"
+                      alt="van_login"
+                    />
+                    <div className="city-type-vehicles  bg-light rounded shadow ">
+                      <p className="mt-3">
+                        {item.name} <br />
+                        <span className="color-subtitle">{item.location}</span>
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
+
+          {/* Cars */}
+          <h3 className="mb-5 mt-5 f-playfair-main">
+            Cars
+            <Link to={`${url}/cars`} className="float-end link-view-all">
+              View all <i className="fas fa-chevron-right text-center ms-2"></i>
+            </Link>
+          </h3>
+
+          {/* Card */}
+          <div className="row position-relative mb-lg-5 wrapper-card">
+            {vehiclesCars.map((item, idx) => {
+              return (
+                <div key={idx} className="col-lg-3 col-md-6 card  ">
+                  <Link
+                    to={`${url}/cars/detail/${item.id}`}
+                    className="wrapper-img-home">
+                    <img
+                      src={`${hostBackend}${item.photo}`}
+                      className="img-size"
+                      alt="van_login"
+                    />
+                    <div className="city-type-vehicles  bg-light rounded shadow ">
+                      <p className="mt-3">
+                        {item.name} <br />
+                        <span className="color-subtitle">{item.location}</span>
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              )
+            })}
           </div>
         </div>
-
-        {/* Popular */}
-        <h3 className="mb-5 mt-5 f-playfair-main">
-          Popular in town
-          <Link to={`${url}/popular`} className="float-end link-view-all">
-            View all <i className="fas fa-chevron-right text-center ms-2"></i>
-          </Link>
-        </h3>
-
-        {/* Card */}
-        <div className="row position-relative mb-lg-5 wrapper-card">
-          {vehiclesPopular.map((item, idx) => {
-            return (
-              <div key={idx} className="col-lg-3 col-md-6 card  ">
-                <Link
-                  to={`${url}/popular/detail/${item.id}`}
-                  className="wrapper-img-home">
-                  <img
-                    src={`${hostBackend}${item.photo}`}
-                    className="img-size"
-                    alt="van_login"
-                  />
-                  <div className="city-type-vehicles  bg-light rounded shadow ">
-                    <p className="mt-3">
-                      {item.vehicle} <br />
-                      <span className="color-subtitle">{item.location}</span>
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            )
-          })}
-        </div>
-
-        {/* Bike */}
-        <h3 className="mb-5 mt-5 f-playfair-main">
-          Bike
-          <Link to={`${url}/bike`} className="float-end link-view-all">
-            View all <i className="fas fa-chevron-right text-center ms-2"></i>
-          </Link>
-        </h3>
-
-        {/* Card */}
-        <div className="row position-relative mb-lg-5 wrapper-card">
-          {vehiclesBike.map((item, idx) => {
-            return (
-              <div key={idx} className="col-lg-3 col-md-6 card  ">
-                <Link
-                  to={`${url}/bike/detail/${item.id}`}
-                  className="wrapper-img-home">
-                  <img
-                    src={`${hostBackend}${item.photo}`}
-                    className="img-size"
-                    alt="van_login"
-                  />
-                  <div className="city-type-vehicles  bg-light rounded shadow ">
-                    <p className="mt-3">
-                      {item.name} <br />
-                      <span className="color-subtitle">{item.location}</span>
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            )
-          })}
-        </div>
-
-        {/* MotorBike */}
-        <h3 className="mb-5 mt-5 f-playfair-main">
-          MotorBike
-          <Link to={`${url}/motorbike`} className="float-end link-view-all">
-            View all <i className="fas fa-chevron-right text-center ms-2"></i>
-          </Link>
-        </h3>
-
-        {/* Card */}
-        <div className="row position-relative mb-lg-5 wrapper-card">
-          {vehiclesMotorBike.map((item, idx) => {
-            return (
-              <div key={idx} className="col-lg-3 col-md-6 card  ">
-                <Link
-                  to={`${url}/motorbike/detail/${item.id}`}
-                  className="wrapper-img-home">
-                  <img
-                    src={`${hostBackend}${item.photo}`}
-                    className="img-size"
-                    alt="van_login"
-                  />
-                  <div className="city-type-vehicles  bg-light rounded shadow ">
-                    <p className="mt-3">
-                      {item.name} <br />
-                      <span className="color-subtitle">{item.location}</span>
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            )
-          })}
-        </div>
-
-        {/* Cars */}
-        <h3 className="mb-5 mt-5 f-playfair-main">
-          Cars
-          <Link to={`${url}/cars`} className="float-end link-view-all">
-            View all <i className="fas fa-chevron-right text-center ms-2"></i>
-          </Link>
-        </h3>
-
-        {/* Card */}
-        <div className="row position-relative mb-lg-5 wrapper-card">
-          {vehiclesCars.map((item, idx) => {
-            return (
-              <div key={idx} className="col-lg-3 col-md-6 card  ">
-                <Link
-                  to={`${url}/cars/detail/${item.id}`}
-                  className="wrapper-img-home">
-                  <img
-                    src={`${hostBackend}${item.photo}`}
-                    className="img-size"
-                    alt="van_login"
-                  />
-                  <div className="city-type-vehicles  bg-light rounded shadow ">
-                    <p className="mt-3">
-                      {item.name} <br />
-                      <span className="color-subtitle">{item.location}</span>
-                    </p>
-                  </div>
-                </Link>
-              </div>
-            )
-          })}
-        </div>
-      </div>
+      ) : (
+        <Loading />
+      )}
 
       <Footer />
     </main>
