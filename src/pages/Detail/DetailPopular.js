f3a384bcfa6d5cb63969da49903151dfd766f987
ikehikeh151 <ikehikeh151@gmail.com>
2022-03-29 15:03:38 +0700

    failed compile is defined but never used detailPopular

--- src/pages/Detail/DetailPopular.js
+++ src/pages/Detail/DetailPopular.js
@@ -5,7 +5,7 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import { getVehiclesPopularApi } from "../../utils/https/vehicles";
+// import { getVehiclesPopularApi } from "../../utils/https/vehicles";
 // import VehiclePopular from "../../components/VehiclePopular"
 
 class Detail extends React.Component {
